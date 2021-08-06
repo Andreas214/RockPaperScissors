6e8b7d226f3653a1af50e2461a09fc815d1190cd
Andreas <andreasroemer92@gmail.com>
2021-08-06 11:02:07 +0200

    updated computerPlay() and added playRound()

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 // Function computerplay
 function computerPlay(){
 // randomly returns Rock, Paper or Scissors
-    let x = ["Rock", "Paper", "Scissors"]
-    let y = Math.floor(Math.random() * 3 + 1);
-    console.log(x[y])
-    
+    let x = Math.floor(Math.random() * 3 + 1);
+
+    if(x === 1){
+        return "Rock"
+    } if (x === 2){
+        return "Paper"
+    } else {return "Scissors"}
 }
 
-computerPlay();
+// Function SingleRound of Rock Paper Scissors
+function playRound(playerSelection, computerSelection){
+// takes in playerSelection and computerSelection
+    if(playerSelection == computerSelection){
+        return "It's a tie! " + computerSelection + " equals " + playerSelection
+    } if(playerSelection === "Rock" && computerSelection === "Scissors" ||  playerSelection === "Scissors" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Rock"){
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    } else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+// returns a string that declares the winner
+}
+
+const playerSelection = "Rock";
+computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection))
